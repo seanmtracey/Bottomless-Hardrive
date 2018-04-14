799deea95cd1621c5f6caab62cd8aa5352499667
seanmtracey <seanmtracey@gmail.com>
2018-04-14 16:16:12 +0100

    Basic read from S3 storage

---
+++ index.js
@@ -0,0 +1,197 @@
+require('dotenv').config();
+const debug = require('debug')('index');
+const fuse = require('fuse-bindings');
+const AWS = require('aws-sdk');
+
+const mountPath = `${__dirname}/mnt`;
+
+const S3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+const cache = {};
+
+fuse.mount(mountPath, {
+	readdir: function (path, cb) {
+		console.log('readdir(%s)', path)
+	//   if (path === '/') return cb(0, ['hello.jpg'])
+	//   cb(0)
+
+		var params = {
+			Bucket: process.env.BUCKET_NAME,
+			Prefix : path === '/' ? '' : path
+		};
+
+		S3.listObjects(params, (err, data) => {
+			if (err) { 
+				debug('listObjects err', err, params); 
+				cb(0);
+			} else {
+				debug(data);
+				const files = data.Contents.map(obj => { return `${obj.Key}` } );
+
+				debug('>>>>>>>>>>>>>> Files:', files);
+				debug('>>>>>>>>>>>>>> Files:', files);
+				debug('>>>>>>>>>>>>>> Files:', files);
+				debug('>>>>>>>>>>>>>> Files:', files);
+				debug('>>>>>>>>>>>>>> Files:', files);
+
+				if(files.length === 0){
+					cb(0);
+				} else {
+					cb(0, files);
+				}
+
+
+			};
+		});
+
+	},
+	getattr: function (path, cb) {
+	  console.log('getattr(%s)', path)
+
+	  S3.headObject({
+			Bucket: process.env.BUCKET_NAME,
+			Key : 'hello.jpg'
+		}, function(err, data) {
+			if (err){
+				debug(`getAttr headObject:`, err); // an error occurred
+				cb(fuse.ENOENT);
+			} else {
+				debug(`S3 Data (${path}):`, data);
+				cb(0, {
+					mtime: new Date(),
+					atime: new Date(),
+					ctime: new Date(),
+					nlink: 1,
+					size: data.ContentLength,
+					mode: path.endsWith('/') ? 16877 : 33188,
+					uid: process.getuid ? process.getuid() : 0,
+					gid: process.getgid ? process.getgid() : 0
+				})
+			}
+		})
+		;
+		
+	//   if (path === '/' || path === '/hello.jpg') {
+	// 	cb(0, {
+	// 	  mtime: new Date(),
+	// 	  atime: new Date(),
+	// 	  ctime: new Date(),
+	// 	  nlink: 1,
+	// 	  size: 100,
+	// 	  mode: 16877,
+	// 	  uid: process.getuid ? process.getuid() : 0,
+	// 	  gid: process.getgid ? process.getgid() : 0
+	// 	})
+	// 	return
+	//   }
+  
+	//   if (path === '/hello.jpg') {
+	// 	cb(0, {
+	// 	  mtime: new Date(),
+	// 	  atime: new Date(),
+	// 	  ctime: new Date(),
+	// 	  nlink: 1,
+	// 	  size: 12,
+	// 	  mode: 33188,
+	// 	  uid: process.getuid ? process.getuid() : 0,
+	// 	  gid: process.getgid ? process.getgid() : 0
+	// 	})
+	// 	return
+	//   }
+  
+	//   cb(fuse.ENOENT)
+	},
+	open: function (path, flags, cb) {
+	  console.log('open(%s, %d)', path, flags)
+	  cb(0, 42) // 42 is an fd
+	},
+	read: function (path, fd, buffer, length, position, cb) {
+	//   console.log('read(%s, %d, %d, %d)', path, fd, len, pos)
+	//   var str = 'hello world\n'.slice(pos, pos + len)
+	//   if (!str) return cb(0)
+	//   buf.write(str)
+	//   return cb(str.length)
+
+		if(path.startsWith('/')){
+			path = path.slice(1, path.length);
+			debug('adj', path);
+			// process.exit();
+		}
+		
+		var params = {
+			Bucket: process.env.BUCKET_NAME,
+			Key: path,
+			// Range : `bytes=${position}-${position + length}`
+		};
+		
+		S3.getObject(params, function(err, data) {
+
+			if(data !== null){
+				debug('dahta:', data);
+				// process.exit();
+			}
+
+
+			// var str = 'hello world\n'.slice(pos, pos + len)
+			
+			if(data){
+				if(data.Body){
+					
+					if(cache[path] === undefined){
+						cache[path] = data.Body;
+					}
+
+					debug('buf:', buffer);
+					console.log('read(%s, %d, %d, %d)', path, buffer, length, position)
+
+					// buf.write(data.Body.slice(pos, pos + len));
+					// return cb(data.Body.length);
+
+					if (position >= data.length){
+						delete cache[path];
+						return cb(0) // done
+					} else {
+						var part = cache[path].slice(position, position + length)
+						part.copy(buffer) // write the result of the read to the result buffer
+						cb(part.length)	
+					}
+
+				}
+			} else {
+				return cb(0)
+			} 
+
+		});
+
+	}
+  }, function (err) {
+	if (err) {
+		debug(err);
+		unmount();
+	}
+	console.log('filesystem mounted on ' + mountPath)
+  })
+
+
+
+function unmount(exit = true){
+	fuse.unmount(mountPath, function (err) {
+		if (err) {
+		  debug('filesystem at ' + mountPath + ' not unmounted', err);
+		} else {
+		  debug('filesystem at ' + mountPath + ' unmounted')
+		}
+		if(exit){
+			process.exit();
+		}
+	});
+}
+process.on('SIGINT', function(err){
+	debug('err:', err);
+	unmount();
+});
+
+process.on('uncaughtException', function(err){
+	debug('err:', err);
+	unmount();
+});
